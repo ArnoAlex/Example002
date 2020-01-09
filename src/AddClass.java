81cf580d668130784c9ee6fbe1b53c98171aebed
ArnoAlex <arnoldalexandryan@gmail.com>
2020-01-09 17:00:45 +0400

    added some changes to this class. TP-148

--- src/AddClass.java
+++ src/AddClass.java
@@ -2,5 +2,6 @@ public class AddClass {
     public static void main(String[] args) {
         System.out.println("Add class");
         System.out.println("naski");
+        System.out.println("swert");
     }
 }
